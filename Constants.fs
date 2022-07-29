dfca987ef195d77ec58daa93013dff7ced53398f
Jannik <sisosys6@gmail.com>
2022-07-29 18:26:22 +0200

    init

---
+++ Constants.fs
@@ -0,0 +1,6 @@
+﻿namespace Test
+
+module Constants =
+    let pgsqlConnectionString (db: string)=
+//        Replace only the password with a real one:
+        $"User ID=postgres;Password=xxx;Host=localhost;Port=5432;Database={db};Pooling=true;Connection Lifetime=0"
